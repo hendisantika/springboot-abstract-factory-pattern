67d6d2ca8ebee6db1a047f1e0fa60a11d8f52028
Hendi Santika <hendisantika@gmail.com>
2020-01-09 05:50:42 +0700

    #1 - Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-abstract-factory-pattern
---
+++ src/main/java/com/hendisantika/springbootabstractfactorypattern/SpringbootAbstractFactoryPatternApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootabstractfactorypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAbstractFactoryPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAbstractFactoryPatternApplication.class, args);
+    }
+
+}
